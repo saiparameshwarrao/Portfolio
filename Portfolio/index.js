a0384107ff45acbac056d634eeb7adb56869e719
Podishetti Sai Parameshwar Rao <134683789+saiparameshwarrao@users.noreply.github.com>
2025-02-13 19:06:53 +0530

    Add files via upload

---
+++ Portfolio/index.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (scrollY >= sectionTop - 60) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
